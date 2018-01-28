5f7de76d8610a529728b3b8dce46931951d61c1e
Alex Huszagh <ahuszagh@ahuszagh.localdomain>
2018-01-28 13:12:54 -0800

    Patches for GCC14, libcxx, and GCC17.

--- pool/allocator.cc
+++ pool/allocator.cc
@@ -1,1 +1,33 @@
+//  :copyright: (c) 2017-2018 Alex Huszagh.
+//  :license: MIT, see licenses/mit.md for more details.
+
+#include "../allocator_test.h"
 #include <pycpp/allocator/pool/allocator.h>
+#include <gtest/gtest.h>
+#include <vector>
+
+PYCPP_USING_NAMESPACE
+
+// TESTS
+// -----
+
+
+TEST(pool, is_relocatable)
+{
+    static_assert(!is_relocatable<pool_allocator<char>>::value, "");
+    static_assert(!is_relocatable<fast_pool_allocator<char>>::value, "");
+}
+
+
+TEST(pool_allocator, pool_allocator)
+{
+    using allocator_type = pool_allocator<char>;
+    allocator_type allocator;
+
+    char* ptr = allocator.allocate(50);
+    allocator.deallocate(ptr, 50);
+
+    ptr = allocator.allocate(50);
+    allocator.deallocate(ptr, 50);
+}
+
--- pool/pool.cc
+++ pool/pool.cc
@@ -1,1 +1,5 @@
+//  :copyright: (c) 2017-2018 Alex Huszagh.
+//  :license: MIT, see licenses/mit.md for more details.
+
 #include <pycpp/allocator/pool/pool.h>
+#include <gtest/gtest.h>
